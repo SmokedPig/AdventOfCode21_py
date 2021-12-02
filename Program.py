7545a381523b231cd259ff7633d7617819a02923
SmokedPig <martin_koelbl@gmx.at>
2021-12-02 22:54:25 +0100

    refactor import and stuff

--- Program.py
+++ Program.py
@@ -1,5 +1,8 @@
-import Solver.Day1 as Day1
+from Solver import *
 
 Day1.Part1()
 Day1.Part2()
+
+Day2.Part1()
+Day2.Part2()
 print("THE END!")
---
+++ Solver/Day2.py
@@ -0,0 +1,35 @@
+# module: Solver for day 2
+solverName = "Day2"
+description = "Solver "+solverName
+filePath = "Inputs\\"+solverName+".txt"
+
+def Part1():
+    print(description+" Part 1")
+
+    # old_value, counter = 0, 0
+    # with open(filePath, "r") as file:
+    #     lines = file.readlines()
+    #     temp = lines[0]
+    #     for x in lines[1:]:
+    #         temp = int(x.rstrip("\n"))
+    #         if old_value < temp:
+    #             counter = counter + 1
+    #         old_value = temp
+
+    # print("Counter: " + str(counter))
+    print(description+" Part 1 Done\n")
+
+def Part2():
+    print(description+" Part 2")
+
+    # counter = 0
+    # with open(filePath, "r") as file:
+    #     lines = file.readlines()
+    #     for x in range(len(lines)-3):
+    #         group_A = sum(int(z) for z in lines[x:x+3])
+    #         group_B = sum(map(int,lines[x+1:x+4]))
+    #         if group_B > group_A:
+    #             counter = counter + 1
+
+    # print("Counter: " + str(counter))
+    print(description+" Part 2 Done\n")
